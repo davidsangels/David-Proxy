e74b063256c72f833a3ceb8b91cf710d7596cc4d
David Brooks <davidbrooks@Davids-MacBook-Pro-2.local>
2019-07-19 19:50:44 -0700

    Add ID

--- server/index.js
+++ server/index.js
@@ -1,25 +1,45 @@
 
 const express = require('express');
-const server = express();
+const app = express();
 const bodyParser = require('body-parser');
-const request = require('request');
+
 var httpProxy = require('http-proxy');
+const reservationServer = 'http://13.57.205.162';
+const reviewsServer = 'http://18.224.181.77';
+const data = 'http://13.56.158.243';
 
 const apiProxy = httpProxy.createProxyServer();
-const data = 'http://localhost:3001'
+
 const port = 3000;
 
-server.use(bodyParser());
-server.use(express.static('public'));
-server.listen(port, () => console.log(`proxy server listening on port : ${port}`));
+app.use(bodyParser());
+app.use('/:id',express.static('public'));
 
-server.get('/data', (req , res) => {
-  console.log('server reached');
+// app.use('/:id'express.static('public'));
+app.listen(port, () => console.log(`proxy server listening on port : ${port}`));
+
+app.get('/bookings/:idPlace', (req, res) => {
+  apiProxy.web(req, res, {target: reservationServer});
+});
+
+
+// Get all the reviews for a specific place
+app.get('/reviews/:idPlace', (req, res) => {
+apiProxy.web(req, res, {target: reviewsServer});
+});
+
+// Get all the ratings for a specific place
+app.get('/ratings/:idPlace', (req, res) => {
+apiProxy.web(req, res, {target: reviewsServer});
+});
+
+// Get all the reviews for a specific query and a specific idPlace
+app.get('/reviews/search/:idPlace/:query', (req, res) => {
+apiProxy.web(req, res, {target: reviewsServer});
+});
+
+//David's image data
+app.get('/data/:id', (req , res) => {
   apiProxy.web(req, res, {target: data});
-
-})
-
-
-
-
-module.exports = server;
+});
+module.exports = app;
